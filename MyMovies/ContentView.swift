0c39f5810457f4c898be557c8a83bedba64fa4cf
mrcormell <d.cormell@etoncollege.org.uk>
2021-06-12 09:25:22 +0100

    Adding image from web api

--- MyMovies/ContentView.swift
+++ MyMovies/ContentView.swift
@@ -5,24 +5,41 @@
 //  Created by Paul Hudson on 12/05/2021.
 //
 
+import SDWebImageSwiftUI
 import SwiftUI
 
 struct ContentView: View {
     @State private var movieResults = Bundle.main.decode(MovieResults.self, from: "results.json", keyDecodingStrategy: .convertFromSnakeCase)
-    
+        
     var body: some View {
         List {
             ForEach(movieResults.results) { movie in
-                VStack(alignment: .leading) {
-                    Text("\(movie.title)")
-                        .font(.title2)
-                    HStack {
-                        Text("\(movie.releaseDate)")
-                        Text("\(movie.voteAverage, specifier: "%g")/10")
+                HStack {
+                    Group {
+                        if let path = movie.posterPath {
+                            WebImage(url: URL(string: "https://image.tmdb.org/t/p/w342\(path)"))
+                                .placeholder(Image("Loading").resizable())
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 60, height: 90)
+                        } else {
+                            Image("NoPoster")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 60, height: 90)
+                        }
                     }
-                    Text("\(movie.overview)")
-                        .lineLimit(2)
-                        .font(Font.body.italic())
+                    VStack(alignment: .leading) {
+                        Text("\(movie.title)")
+                            .font(.title2)
+                        HStack {
+                            Text("\(movie.releaseDate)")
+                            Text("\(movie.voteAverage, specifier: "%g")/10")
+                        }
+                        Text("\(movie.overview)")
+                            .lineLimit(2)
+                            .font(Font.body.italic())
+                    }
                 }
             }
         }
